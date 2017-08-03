a6b99e3106530e4a7cb6db37c6e451f64255ab18
CourseIng <courseing@gmail.com>
2017-08-03 17:02:53 +0530

    Create 004_Notify.pp

---
+++ examples/004_Notify.pp
@@ -0,0 +1,3 @@
+package { 'mysql-server':  ensure => installed,  notify => Service['mysql'],}
+file { '/etc/mysql/mysql.cnf':  source => '/vagrant/examples/files/mysql.cnf',  notify => Service['mysql'],}
+service { 'mysql':  ensure => running,  enable => true,}
