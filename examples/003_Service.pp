6f30490f2f0fdea76651f8e08c419ef8dfe44b97
CourseIng <courseing@gmail.com>
2017-08-03 16:58:28 +0530

    Update 003_Service.pp

--- examples/003_Service.pp
+++ examples/003_Service.pp
@@ -1,4 +1,7 @@
 service { 'sshd':  ensure => running,  enable => true,}
 
 #Description of the service resource, along with a complete list of attributes and allowed values.
-#sudo puppet describe service
+sudo puppet describe service
+
+# To List out all available resource types
+sudo puppet describe --list
